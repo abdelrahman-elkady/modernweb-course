1b2706a22fdc20259052b131bcf1a3052233e468
kady <elkadyabdelrahman@gmail.com>
2017-03-27 10:24:02 +0200

    Fix Header component styling

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import Header from './components/Header/Header';
+
 import './index.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
 
-      <div id="appContainer" className="col">
-        <header className="header row primary-bg-color">
-          <h1 className="header__title">To Do App</h1>
-        </header>
-
-        {/* <div className="row">
-          <TodoListList />
-          <TodoList />
-        </div> */}
+        <Header/> {/* <div className="row">
+        <TodoListList />
+        <TodoList />
+      </div> */}
       </div>
     );
   }
--- todo-react/src/components/Header/Header.js
+++ todo-react/src/components/Header/Header.js
@@ -1,9 +1,13 @@
+import React from 'react';
+
 import './header.css';
 
-export default const Header = props => {
+const Header = props => {
   return (
     <header className="header row primary-bg-color">
       <h1 className="header__title">To Do App</h1>
     </header>
   );
 }
+
+export default Header;
