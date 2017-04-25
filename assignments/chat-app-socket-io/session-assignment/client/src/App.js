5d253dc7b2297f6c3a8116845e253c3b5dd1a5eb
kady <elkadyabdelrahman@gmail.com>
2017-04-25 14:19:21 +0200

    Basic socket IO integration

--- assignments/chat-app-socket-io/session-assignment/client/src/App.js
+++ assignments/chat-app-socket-io/session-assignment/client/src/App.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
 
 class App extends Component {
+
+  connect() {
+    let socket = io('localhost:3000');
+  }
+
   render() {
     return (
       <div>
+        <button onClick={() => this.connect()}>Connect</button>
         <ul id="messages"></ul>
         <form action="">
           <input id="m" autocomplete="off"/>
